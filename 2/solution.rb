9698b66c09bdd6c80b6c4cbf7ae65cda6f667e97
Andrew Jacobson <andyjacuw@gmail.com>
2015-12-14 15:32:36 -0800

    Ruby solution for 2 - 2

--- 2/solution.rb
+++ 2/solution.rb
@@ -18,8 +18,6 @@ def calculate_wrapping_paper_sum(input)
   wrapping_paper_sum
 end
 
-# helpers
-
 def calculate_wrapping_paper_per_present(spec)
   side_one = spec[:length] * spec[:width]
   side_two = spec[:width] * spec[:height]
@@ -28,6 +26,47 @@ def calculate_wrapping_paper_per_present(spec)
 
   (2 * side_one) + (2 * side_two) + (2 * side_three) + smallest_side
 end
+
+# part 2
+
+def calculate_ribbon_sum(input)
+  presents = parse_presents(input)
+  ribbon_sum = 0
+
+  presents.each do |present|
+    present_spec = parse_present_dimension(present)
+
+    ribbon_sum += calculate_ribbon_sum_per_present(present_spec)
+  end
+
+  ribbon_sum
+end
+
+def calculate_ribbon_sum_per_present(spec)
+  sides = [spec[:length], spec[:width], spec[:height]]
+  smallest_two_sides = find_smallest_two_sides(sides)
+  smallest_perimeter = calculate_smallest_perimeter(smallest_two_sides)
+  bow = spec[:length] * spec[:width] * spec[:height]
+
+  smallest_perimeter + bow
+end
+
+def find_smallest_two_sides(sides)
+  sorted = sides.sort do |a, b|
+    a - b
+  end
+
+  [sorted[0], sorted[1]]
+end
+
+def calculate_smallest_perimeter(sides)
+  side_one = sides[0]
+  side_two = sides[1]
+
+  side_one + side_one + side_two + side_two
+end
+
+# helpers
 
 def parse_presents(present_string)
   presents = present_string.split("\n")
